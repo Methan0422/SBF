f7dd842a3f23f6d7e229a8ce014778dd35350e7d
Methan0422 <berard.ethan@gmail.com>
2024-09-09 06:27:49 +0000

    stuf is starting to work kinda

--- Main.js
+++ Main.js
@@ -1,3 +1,4 @@
+//calling from css
 let dom_solve = document.querySelector("#solve");
 
 
@@ -5,27 +6,39 @@ let grid = [];
 let segOne = [];
 let abc = ['a', 'b', 'c', 'd', 'e', 'g', 'h', 'i', 'j'];
 
+//The solve button
 dom_solve.addEventListener("click", () => {
-    let x = document.forms["formOne"]["inputOne"].value;
-    if (x === '') {
-        alert("worked");
-    }
+    getForm();
+
+
+//grid system for input boxes
+/*    for (let i = 0; i < 3; i++) {
+        let row = [];
+        for (let j = 0; j < 3; j++) {
+            alert(abc[i] + abc[j]);
+            }}
+*/
 });
 
+//makes list of values
 function getForm(){
     let segOne = [];
     for (let i = 0; i < 3; i++) {
         let row = [];
         for (let j = 0; j < 3; j++) {
-            let x = document.forms[[i][j]][[i][j]].value;
+            let x = document.forms[abc[i] + abc[j]][abc[i] + abc[j]].value;
+            alert("start");
             if (x === ''){
-                row.push
+                row.push('0');
             }
-
-            alert(abc[j][i]);
-
-            //let x = document.forms[[i][j]][[i][j]].value;
+            if (x > 0){
+                row.push(x);
+            }
+        }
+        alert("close");
+        segOne.push(row);
     }
-
+    alert("Made it");
+    grid.push(segOne);
 }
-//let x = document.forms["formOne"]["inputOne"].value;//
+//let x = document.forms["formOne"]["inputOne"].value;
