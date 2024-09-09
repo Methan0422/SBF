9041fb1321028717c30bca51f27b074da466ec7f
Methan0422 <berard.ethan@gmail.com>
2024-09-09 05:38:31 +0000

    starting recording

--- Main.js
+++ Main.js
@@ -1,1 +1,20 @@
 let dom_solve = document.querySelector("#solve");
+
+
+let grid = [];
+let abc = ['a', 'b', 'c', 'd', 'e', 'g', 'h', 'i', 'j']
+
+dom_solve.addEventListener("click", () => {
+    getForm();
+});
+
+function getForm(){
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            alert(abc[i][j]);
+            //let x = document.forms[[i][j]][[i][j]].value;//
+        }
+    }
+
+}
+//let x = document.forms["formOne"]["inputOne"].value;//
