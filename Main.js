739125863740a482a68543c87293292afc5f4493
Methan0422 <berard.ethan@gmail.com>
2024-09-09 06:02:06 +0000

    more stuff

--- Main.js
+++ Main.js
@@ -2,18 +2,29 @@ let dom_solve = document.querySelector("#solve");
 
 
 let grid = [];
-let abc = ['a', 'b', 'c', 'd', 'e', 'g', 'h', 'i', 'j']
+let segOne = [];
+let abc = ['a', 'b', 'c', 'd', 'e', 'g', 'h', 'i', 'j'];
 
 dom_solve.addEventListener("click", () => {
-    getForm();
+    let x = document.forms["formOne"]["inputOne"].value;
+    if (x === '') {
+        alert("worked");
+    }
 });
 
 function getForm(){
+    let segOne = [];
     for (let i = 0; i < 3; i++) {
+        let row = [];
         for (let j = 0; j < 3; j++) {
-            alert(abc[i][j]);
-            //let x = document.forms[[i][j]][[i][j]].value;//
-        }
+            let x = document.forms[[i][j]][[i][j]].value;
+            if (x === ''){
+                row.push
+            }
+
+            alert(abc[j][i]);
+
+            //let x = document.forms[[i][j]][[i][j]].value;
     }
 
 }
