b56650ce5473c2b073e7edc0de79829c5069efe9
Methan0422 <berard.ethan@gmail.com>
2024-09-13 14:06:21 -0700

    appeance stuff

--- Main.js
+++ Main.js
@@ -88,13 +88,14 @@ function getForm(){
     }
 }
 
+/*
 function lockGrid(){
     for (let i = 0; i < 9; i++){
         for (let j = 0; j < 9; j++){
             document.getElementById(abc[i] + abc[j]).disable = true;
         }
     }
-}
+}*/
 
 function rowCheck(){
     let tempRow = [];
